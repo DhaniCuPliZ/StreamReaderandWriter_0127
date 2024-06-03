c98c1a639a60051b77bccbe6d3cadd8a825fc73e
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2024-06-03 10:29:40 +0700

    exception menangkap bahwa array hanya memiliki 3 elemen, tapi program tetap berjalan

--- Exception/Exception.cpp
+++ Exception/Exception.cpp
@@ -14,4 +14,11 @@ int main()
 		cout << data.at(5) << endl;
 		//memanggil array elemen ke 5
 	}
+	catch (exception& e) {
+		//penangkap menggunakan obyek exception
+		cout << e.what() << endl;
+		/*akan dieksekusi karna array data hanya memiliki 3 elemen*/
+	}
+	cout << "Baris Program Yang Terakhir" << endl;
+	/*penanda 2: Bahwa program berjalan tanpa berhenti*/
 }
